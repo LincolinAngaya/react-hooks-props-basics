30c73773914c47d5d73e587e3c4e21f1155573c5
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-13 12:49:08 +0300

    practice on props

---
+++ src/components/BlogpostMain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogPost from "./Blogpost";
+
+const BlogpostMain = () => {
+    return ( <>
+    <BlogPost />
+    
+        </>
+    )
+}
+
+export default BlogpostMain;
